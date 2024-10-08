ffcf2dbc515a2a5e8d891b258b6522635444620e
Oleksii Stepanov <lllstepanovwork@gmail.com>
2024-10-08 14:02:40 +0200

    feat: Create MainView script
    - Create MainView script
    - Add Constants script
    - Add GoToN1Site Button

---
+++ n1-test-assignment-unity/Assets/N1TestAssignment/Scripts/UI/MainView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using N1TestAssignment.Data;
+
+namespace N1TestAssignment.UI
+{
+    public class MainView : MonoBehaviour
+    {
+        public void OpenN1Site()
+        {
+            
+        }
+    }
+}
